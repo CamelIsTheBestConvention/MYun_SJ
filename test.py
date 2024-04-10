a91c35b8118b4487e7e0e152fc495ffcd58a775c
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-10 15:09:02 +0900

    baechu

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-n = 7
-def solution(n):
-    answer = 0
-    a = 1
-    while True:
-        for i in range(1,n+1):
-            a *= i
-            print(a)
-            answer += 1
-            if a > n:
-                return answer - 1
-            if a == n:
-                return answer
-        break
-    return answer-1
-print(solution(n))
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def dfs(x, y):
+    if x < 0 or x >= a or y < 0 or y >= b:
+        return False
+    if visited[x][y] == 1:
+        visited[x][y] = 0
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    baechu = []
+    visited = [[0]*b for _ in range(a)]
+    for j in range(c):
+        x, y = map(int, input().split())
+        baechu.append((x, y))
+        visited[x][y] = 1
+    cnt = 0
+    for x, y in baechu:
+        if dfs(x, y):
+            cnt += 1
+    print(cnt)
